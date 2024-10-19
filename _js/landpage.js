4c7d6077a3780ab08ce56c46d87be9c82642800d
[Chrystian Almeida] <[Chrystian.almeida@alunoescolassociais.org.br]>
2024-10-19 20:19:48 -0300

    Escutador do tipo click, que direciona o usuário para a página de cadastro de usuário

---
+++ _js/landpage.js
@@ -0,0 +1,5 @@
+// Aqui o escutador indentifica se o botão foi clicado("focus") e executa a função entrar 
+document.getElementById("btn-cad").addEventListener(("focus"), entrar);
+function entrar() {
+    window.location.href = "cad.html"; //*endereço da página*//
+}
