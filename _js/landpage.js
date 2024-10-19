f0e81b7dfd0adedca18ba137176941474cf50686
[Chrystian Almeida] <[Chrystian.almeida@alunoescolassociais.org.br]>
2024-10-19 20:22:38 -0300

    Adiciona animação a caixa de pesquisa dentro do header

--- _js/landpage.js
+++ _js/landpage.js
@@ -3,3 +3,21 @@ document.getElementById("btn-cad").addEventListener(("focus"), entrar);
 function entrar() {
     window.location.href = "cad.html"; //*endereço da página*//
 }
+
+// Chama as divs do projeto
+let barraBusca = document.querySelector(".barra-busca");
+let pesquisarBox = document.querySelector(".pesquisar-box");
+let closeBox = document.querySelector(".close-box");
+
+// Arrow function que adiciona a personalização CSS "Abrir", após o click na div citada
+pesquisarBox.addEventListener("click", () => {
+    barraBusca.classList.add('abrir');
+    document.getElementById("buscaInput").value = "";
+})
+// Arrow function que remove a personalização CSS "Abrir", após o click na div citada
+
+closeBox.addEventListener("click", () => {
+    barraBusca.classList.remove('abrir');
+
+})
+
